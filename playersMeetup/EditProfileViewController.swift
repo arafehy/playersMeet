b3a8a3147b701513a643b24cb9372ebc4fae0939
Yazan Arafeh <yazanarafeh24@gmail.com>
2020-05-03 16:29:13 -0700

    Created EditProfileVC and connected outlets

---
+++ playersMeetup/EditProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateProfileViewController.swift
+//  playersMeetup
+//
+//  Created by Yazan Arafeh on 5/2/20.
+//  Copyright © 2020 Nada Zeini. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var bioTextView: UITextView!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
