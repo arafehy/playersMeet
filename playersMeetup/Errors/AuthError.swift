5c13447fbcd59ebf7acfb6210f4ab5507eeddb3b
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-10-30 20:17:55 -0700

    Corrected error handling on create/signIn user

---
+++ playersMeetup/Errors/AuthError.swift
@@ -0,0 +1,13 @@
+//
+//  AuthError.swift
+//  playersMeetup
+//
+//  Created by Yazan Arafeh on 10/30/21.
+//  Copyright © 2021 Nada Zeini. All rights reserved.
+//
+
+import Foundation
+
+enum AuthError: Error {
+    case userNotFound
+}
