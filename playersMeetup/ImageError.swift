30e5144ef1db697c95089a584727f765eff21914
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-10-30 19:34:06 -0700

    Refactored profile picture loading

---
+++ playersMeetup/ImageError.swift
@@ -0,0 +1,13 @@
+//
+//  ImageError.swift
+//  playersMeetup
+//
+//  Created by Yazan Arafeh on 10/30/21.
+//  Copyright © 2021 Nada Zeini. All rights reserved.
+//
+
+import Foundation
+
+enum ImageError: Error {
+    case invalidData
+}
