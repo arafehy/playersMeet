467ebcbfadc23c9e7e5fe10d87afec22edb4d82a
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-10-31 18:05:10 -0700

    Added DatabaseError type

---
+++ playersMeetup/Errors/DatabaseError.swift
@@ -0,0 +1,13 @@
+//
+//  DatabaseError.swift
+//  playersMeetup
+//
+//  Created by Yazan Arafeh on 10/31/21.
+//  Copyright © 2021 Nada Zeini. All rights reserved.
+//
+
+import Foundation
+
+enum DatabaseError: Error {
+    case invalidInput
+}
