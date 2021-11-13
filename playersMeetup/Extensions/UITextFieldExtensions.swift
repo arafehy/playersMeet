8f7833794802a5bcea412bfea45e48cecbfeb025
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-11-13 15:40:28 -0800

    Changed order of property setting for consistency

--- playersMeetup/Extensions/UITextFieldExtensions.swift
+++ playersMeetup/Extensions/UITextFieldExtensions.swift
@@ -11,12 +11,11 @@ import UIKit
 extension UITextField {
     func rounded() {
         self.layer.cornerRadius = 25
-        self.layer.borderColor = UIColor.white.cgColor
         self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.white.cgColor
     }
     
     func configure() {
-        self.layer.zPosition = 1
         self.layer.cornerRadius = 10
         self.layer.borderWidth = 1
         self.layer.borderColor = UIColor.white.cgColor
