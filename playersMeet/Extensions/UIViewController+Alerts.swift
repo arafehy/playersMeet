d81be85515c4b010f92c394cd2e039a825990fdc
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-11-21 23:34:47 -0800

    Simplified code for alerts

--- playersMeet/Extensions/UIViewController+Alerts.swift
+++ playersMeet/Extensions/UIViewController+Alerts.swift
@@ -10,11 +10,11 @@ import UIKit
 
 extension UIViewController {
     func showErrorAlert(with error: Error?) {
-        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alertController.addAction(defaultAction)
+        alert.addAction(defaultAction)
         DispatchQueue.main.async {
-            self.present(alertController, animated: true, completion: nil)
+            self.present(alert, animated: true)
         }
     }
 }
@@ -23,10 +23,12 @@ extension EditProfileViewController {
     func showCancelProfileUpdateAlert() {
         let alert = UIAlertController(title: "Are you sure you want to cancel?", message: "There are unsaved changes that will be deleted.", preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Discard Changes", style: .destructive, handler: { _ in
-            self.dismiss(animated: true, completion: nil)
+            self.dismiss(animated: true)
         }))
         alert.addAction(UIAlertAction(title: "Continue Updating Profile", style: .cancel, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+        }
     }
 }
 
