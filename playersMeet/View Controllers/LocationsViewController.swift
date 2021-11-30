7305ad83e2342885d8b34cc922f22342f4bdc4fc
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-11-29 17:36:43 -0800

    Added missing call to super in viewWillAppear

--- playersMeet/View Controllers/LocationsViewController.swift
+++ playersMeet/View Controllers/LocationsViewController.swift
@@ -38,6 +38,7 @@ class LocationsViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         tableView.reloadData()
     }
     
