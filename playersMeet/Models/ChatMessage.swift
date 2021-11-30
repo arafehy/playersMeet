1b05c84361e1872af7858b3c9238f8bdad3cad6b
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-11-30 13:40:46 -0800

    Created ChatMessage model

---
+++ playersMeet/Models/ChatMessage.swift
@@ -0,0 +1,18 @@
+//
+//  ChatMessage.swift
+//  playersMeet
+//
+//  Created by Yazan Arafeh on 11/30/21.
+//  Copyright © 2021 Yazan Arafeh. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct ChatMessage: Codable {
+    let userID: String
+    let username: String
+    let text: String
+    let createdAt: TimeInterval
+    let color: String
+}
