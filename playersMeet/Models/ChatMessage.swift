0c35feb4a7637656843b51cb3ff58219878e75ed
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-11-30 23:47:22 -0800

    Updated ChatMessage to convert to dictionary before sending message

--- playersMeet/Models/ChatMessage.swift
+++ playersMeet/Models/ChatMessage.swift
@@ -15,4 +15,13 @@ struct ChatMessage: Codable {
     let text: String
     let createdAt: TimeInterval
     let color: String
+    
+    func asDictionary() -> [String: Any] {
+        return ["userID": userID,
+                "username": username,
+                "text": text,
+                "createdAt": createdAt,
+                "color": color
+        ]
+    }
 }
