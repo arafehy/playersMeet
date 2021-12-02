2bdce36c9029b76fa43f774b3414baa128929c38
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-12-01 23:17:07 -0800

    Started refactoring message cell configuration code

--- playersMeet/Views/MessageCell.swift
+++ playersMeet/Views/MessageCell.swift
@@ -27,4 +27,28 @@ class MessageCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func configure(with message: ChatMessage) {
+        if (Auth.auth().currentUser!.uid == (message.userID)) {
+            self.nameLabel.text = "\(message.username) (Me)"
+            self.nameLabel.textColor = UIColor.orange
+        } else {
+            self.nameLabel.text = message.username
+            let col: String = message.color
+            if col == "#000000"{
+                let uiColor: UIColor = UIColor(hexString: "#808080")
+                self.nameLabel.textColor = uiColor
+            }
+            else{
+                let uiColor: UIColor = UIColor(hexString: col)
+                self.nameLabel.textColor = uiColor
+            }
+        }
+        
+        msgLabel.text = message.text
+        createdAtLabel.text = Formatter.getReadableDate(timeInterval: message.createdAt)
+        tapRecognizer.addTarget(self, action: #selector(showProfile))
+        tapRecognizer.userID = message.userID
+        nameLabel.gestureRecognizers = []
+        nameLabel.gestureRecognizers!.append(tapRecognizer)
+    }
 }
