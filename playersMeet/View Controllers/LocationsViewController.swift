a6cdd30844fb7485ec006eb0f22a5830eb842bf9
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-12-02 22:27:34 -0800

    Changed how LocationCell receives its location property for configuration

--- playersMeet/View Controllers/LocationsViewController.swift
+++ playersMeet/View Controllers/LocationsViewController.swift
@@ -92,7 +92,7 @@ extension LocationsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let locationCell = tableView.dequeueReusableCell(withIdentifier: "LocationTableViewCell") as! LocationCell
         let location = locations[indexPath.row]
-        locationCell.configure(with: location)
+        locationCell.location = location
         return locationCell
     }
     
