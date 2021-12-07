50e72091d19f49c593485a0192409a879fcdaf37
Yazan Arafeh <yazanarafeh24@gmail.com>
2021-12-06 23:09:21 -0800

    Refactored formatters to be configured once on initialization

--- playersMeet/Helpers/Formatter.swift
+++ playersMeet/Helpers/Formatter.swift
@@ -9,18 +9,24 @@
 import Foundation
 
 struct Formatter {
-    private static let measurementFormatter = MeasurementFormatter()
-    private static let dateFormatter = DateFormatter()
+    private static let measurementFormatter: MeasurementFormatter = {
+        let formatter = MeasurementFormatter()
+        formatter.unitOptions = .naturalScale
+        return formatter
+    }()
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.doesRelativeDateFormatting = true
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter
+    }()
     
     static func getReadableMeasurement(_ measurement: Measurement<UnitLength>) -> String {
-        measurementFormatter.unitOptions = .naturalScale
         return measurementFormatter.string(from: measurement)
     }
     
     static func getReadableDate(timeInterval: TimeInterval) -> String {
-        dateFormatter.doesRelativeDateFormatting = true
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .short
         return dateFormatter.string(from: Date(timeIntervalSince1970: timeInterval))
     }
 }
